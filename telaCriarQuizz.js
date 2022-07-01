63d57592389ccae933e561574521b06341641d19
belleomoraes <iolliveira.moraes@gmail.com>
2022-06-30 21:27:34 -0300

    com layout para criação de quizz

---
+++ telaCriarQuizz.js
@@ -0,0 +1,94 @@
+function criarQuizz() {
+  const BodyDiv = document.querySelector(".main");
+  BodyDiv.innerHTML = "";
+  const criacaoQuizz = `
+  <div class="criarQuizz criacaoTela1">
+  <h1>Comece pelo começo</h1>
+  <div class="esqueletoQuizz">
+  <input type="text" placeholder="Título do seu quizz" />
+  <input type="text" placeholder="URL da imagem do seu quizz" />
+  <input type="text" placeholder="Quantidade de perguntas do quizz" />
+  <input type="text" placeholder="Quantidade de níveis do quizz" />
+  </div>
+  <div class="prosseguir" onclick="criarPerguntas()">Prosseguir para criar perguntas</div>
+  </div> 
+    `;
+    BodyDiv.innerHTML = criacaoQuizz;
+}
+
+function criarPerguntas() {
+  const BodyDiv = document.querySelector(".main");
+  BodyDiv.innerHTML = "";
+
+  const criacaoPerguntas = `
+  <div class="criarQuizz criacaoTela2">
+  <h1>Crie suas perguntas</h1>
+  <div class="perguntas">
+  <span>Pergunta 1</span>
+  <input type="text" placeholder="Texto da pergunta" />
+  <input type="text" placeholder="Cor de fundo da pergunta" />
+  <span>Resposta correta</span>
+  <input type="text" placeholder="Resposta correta" />
+  <input type="text" placeholder="URL da imagem" />
+  <span>Respostas incorretas</span>
+  <input type="text" placeholder="Resposta incorreta 1" />
+  <input type="text" placeholder="URL da imagem 1" />
+  <input type="text" placeholder="Resposta incorreta 2" />
+  <input type="text" placeholder="URL da imagem 2" />
+  <input type="text" placeholder="Resposta incorreta 3" />
+  <input type="text" placeholder="URL da imagem 3" />
+  </div>
+  <div class="prosseguir" onclick="criarNiveis()">Prosseguir para criar níveis</div>
+  </div> 
+    `;
+    BodyDiv.innerHTML = criacaoPerguntas;
+}
+
+function criarNiveis() {
+  const BodyDiv = document.querySelector(".main");
+  BodyDiv.innerHTML = "";
+
+    const criacaoNiveis = `
+    <div class="criarQuizz criacaoTela3">
+    <h1>Agora, crie níveis</h1>
+    <div class="niveis">
+    <span>Nivel 1</span>
+    <input type="text" placeholder="Título do nível" />
+    <input type="text" placeholder="% acerto mínima" />
+    <input type="text" placeholder="URL da imagem do nível" />
+    <input type="text" placeholder="Descrição do nível" />
+    </div>
+    <div class="prosseguir" onclick="finalizarQuizz()">Finalizar Quizz</div>
+    </div> 
+      `;
+      BodyDiv.innerHTML = criacaoNiveis;
+  }
+
+  function finalizarQuizz() {
+    const BodyDiv = document.querySelector(".main");
+  BodyDiv.innerHTML = "";
+
+    
+    const novoQuizz = `
+    <div class="criarQuizz criacaotela4">
+    <h1>Seu quizz está pronto!</h1>
+    <div class="quizzCriado">
+    <img src="img/Rectangle 36.png"/>
+    
+    </div>
+    <div class="prosseguir" onclick="acessarQuizz()">Acessar Quizz</div>
+    <h2 onclick="voltarHome()">Voltar para home </h2>
+    </div> 
+      `;
+
+    BodyDiv.innerHTML = novoQuizz;
+
+  }
+
+  function acessarQuizz () {
+
+  }
+
+  function voltarHome () {
+    window.location.reload()
+  }
--- telaEntrada.js
+++ telaEntrada.js
@@ -1,40 +1,34 @@
 let listaQuizz1 = [];
 let listaQuizzUsuário1 = [];
 
-
 function pegarQuizzAPI1() {
-    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-    promise.catch(erroNaAPi1);
-    promise.then(popularListas1);
+  const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+  promise.catch(erroNaAPi1);
+  promise.then(popularListas1);
 }
 
-
-pegarQuizzAPI1()
-
+pegarQuizzAPI1();
 
 function erroNaAPi1() {
-    alert('Ocorreu um erro, os quizz não foram encontrados na API');
+  alert("Ocorreu um erro, os quizz não foram encontrados na API");
 }
-
 
 function popularListas1(promise) {
-    listaQuizz1 = promise.data;
-    renderizarTelaInicial1()
+  listaQuizz1 = promise.data;
+  renderizarTelaInicial1();
 }
-
 
 function renderizarTelaInicial1() {
-    if (listaQuizzUsuário1.length === 0) {
-        mostrarTelaSemQuizz1();
-    } else {
-        mostrarTelaCompleta1();
-    }
+  if (listaQuizzUsuário1.length === 0) {
+    mostrarTelaSemQuizz1();
+  } else {
+    mostrarTelaCompleta1();
+  }
 }
 
-
 function mostrarTelaSemQuizz1() {
-    const bodyDiv = document.querySelector('body');
-    const templateBody =`
+  const bodyDiv = document.querySelector("body");
+  const templateBody = `
         <div class="header">
             <h1>BuzzQuizz</h1>
         </div>
@@ -43,7 +37,7 @@ function mostrarTelaSemQuizz1() {
                 <div class="usuario-quizz">
                     <div class="titulo-botao">
                         <h2>Você não criou nenhum </br>quizz ainda :(</h2>
-                        <button class="criar-quizz-g" onclick="criarQuizz1()">Criar Quizz</button>
+                        <button class="criar-quizz-g" onclick="criarQuizz1(this)">Criar Quizz</button>
                     </div>
                 </div>
             </div>
@@ -51,28 +45,27 @@ function mostrarTelaSemQuizz1() {
                 <h3>Todos os Quizzes</h3>
                 <div class="quizz-container"></div>
             </div>
-        </div>`
+        </div>`;
 
-    bodyDiv.innerHTML += templateBody;
+  bodyDiv.innerHTML += templateBody;
 
-    for (let i=0; i<listaQuizz1.length; i++){
-        let iterarQuizz = listaQuizz1[i];
-        const quizContainerDiv = document.querySelector('.quizz-container');
-        const templateQuizz = `
+  for (let i = 0; i < listaQuizz1.length; i++) {
+    let iterarQuizz = listaQuizz1[i];
+    const quizContainerDiv = document.querySelector(".quizz-container");
+    const templateQuizz = `
         <div class="quizz" onclick="abrirQuizz1()">
             <img src=${iterarQuizz.image}/>
             <div class="titulo-quizz">
                 <p>${iterarQuizz.title}</p>
             </div>
-        </div>`
-        quizContainerDiv.innerHTML += templateQuizz;
-    }
+        </div>`;
+    quizContainerDiv.innerHTML += templateQuizz;
+  }
 }
 
-
-function  mostrarTelaCompleta1(){
-    const bodyDiv = document.querySelector('body');
-    const templateBody =`
+function mostrarTelaCompleta1() {
+  const bodyDiv = document.querySelector("body");
+  const templateBody = `
         <div class="header">
             <h1>BuzzQuizz</h1>
         </div>
@@ -90,45 +83,39 @@ function  mostrarTelaCompleta1(){
                 <h3>Todos os Quizzes</h3>
                 <div class="quizz-container"></div>
             </div>
-        </div>`
+        </div>`;
 
-    bodyDiv.innerHTML += templateBody;
+  bodyDiv.innerHTML += templateBody;
 
-    for (let i=0; i<listaQuizzUsuário1.length; i++){
-        let iterarQuizz = listaQuizzUsuário1[i];
-        const quizContainerDiv = document.querySelector('.meu-quizz-container');
-        const templateQuizz = `
+  for (let i = 0; i < listaQuizzUsuário1.length; i++) {
+    let iterarQuizz = listaQuizzUsuário1[i];
+    const quizContainerDiv = document.querySelector(".meu-quizz-container");
+    const templateQuizz = `
         <div class="quizz" onclick="abrirQuizz1()">
             <img src=${iterarQuizz.image}/>
             <div class="titulo-quizz">
                 <p>${iterarQuizz.title}</p>
             </div>
-        </div>`
-        quizContainerDiv.innerHTML += templateQuizz;
-    }
-
+        </div>`;
+    quizContainerDiv.innerHTML += templateQuizz;
+  }
 }
 
+function abrirQuizz1() {
+  const BodyDiv = document.querySelector("body");
+  BodyDiv.innerHTML = "";
 
-
-function abrirQuizz1(){
-    const BodyDiv = document.querySelector('body');
-    BodyDiv.innerHTML = "";
-
-    //CHAMAR FUNÇÃO QUE APARECE A TELA DO QUIZZ: PENDING!
+  //CHAMAR FUNÇÃO QUE APARECE A TELA DO QUIZZ: PENDING!
 }
 
+function criarQuizz1(elemento) {
+const BodyDiv = document.querySelector(".main");
+BodyDiv.innerHTML = "";
+  criarQuizz();
 
-function criarQuizz1(){
-    const BodyDiv = document.querySelector('body');
-    BodyDiv.innerHTML = "";
-    //CHAMAR FUNÇÃO QUE APARECE A TELA DE CRIAR QUIZZ: PENDING!
+  //BodyDiv.innerHTML = "";
+  //CHAMAR FUNÇÃO QUE APARECE A TELA DE CRIAR QUIZZ: PENDING!
 }
-
-
-
-
-
 
 
 /* <div class="main">
